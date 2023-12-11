4d2a34acf4cd3c1de2c629fd0af90012d5d31e96
rickyarmnd <rickyarmnd@gmail.com>
2023-12-12 05:05:08 +0700

    rename class

--- cnn-lstm/model.py
+++ cnn-lstm/model.py
@@ -8,7 +8,7 @@ from PIL import ImageFile
 
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-class model:
+class Model:
     #PARENT DIRECTORY
     PARENT_DIRECTORY = os.getcwd()
     DATASET_PATH = os.path.join(PARENT_DIRECTORY,'cnn-lstm' , 'split-dataset')
@@ -103,4 +103,4 @@ class model:
 
         return model
 
-model()
+Model()
